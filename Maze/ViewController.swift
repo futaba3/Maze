b2db2f810537aec0ff5efd449d39dae693a3dd78
Futaba3 <nyokinyoki41aya@gmail.com>
2022-05-13 12:47:06 +0900

    commit

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -6,14 +6,177 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    // 画面サイズの取得
+    let screenSize = UIScreen.main.bounds.size
+    
+    // 迷路のマップを表す配列
+    let maze = [
+        [1, 0, 0, 0, 1, 0],
+        [1, 0, 1, 0, 1, 0],
+        [3, 0, 1, 0, 1, 0],
+        [1, 1, 1, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1],
+        [0, 1, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 2],
+    ]
+    
+    var startView: UIView!
+    var goalView: UIView!
+    var playerView: UIView!
+    
+    // CMMotionManagerはiPhoneの動きを感知する
+    var playerMotionManager: CMMotionManager!
+    var speedX: Double = 0.0
+    var speedY: Double = 0.0
+    
+    // wallViewのフレームの情報を入れる配列
+    var wallRectArray = [CGRect]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // 常にライトモード（明るい外観）を指定することでダークモード適用を回避
+        self.overrideUserInterfaceStyle = .light
+        
+        // マップ1マスの幅と高さ
+        let cellWidth = screenSize.width / CGFloat(maze[0].count)
+        let cellHeight = screenSize.height / CGFloat(maze.count)
+        // マスの左上とマスの中心のx/y座標の差
+        let cellOffsetX = cellWidth / 2
+        let cellOffsetY = cellHeight / 2
+
+        // Viewの生成
+        for y in 0 ..< maze.count {
+            for x in 0 ..< maze[y].count {
+                switch maze[y][x] {
+                //当たるとゲームオーバー
+                case 1:
+                    let wallView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    wallView.backgroundColor = UIColor.black
+                    view.addSubview(wallView)
+                    wallRectArray.append(wallView.frame)
+                // スタート地点
+                case 2:
+                    startView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    startView.backgroundColor = UIColor.green
+                    view.addSubview(startView)
+                // ゴール地点
+                case 3:
+                    goalView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    goalView.backgroundColor = UIColor.red
+                    view.addSubview(goalView)
+                default:
+                    break
+                }
+            }
+        }
+        
+        // playerViewを生成
+        playerView = UIView(frame: CGRect(x: 0, y: 0, width: cellWidth / 6, height: cellHeight / 6))
+        playerView.center = startView.center
+        playerView.backgroundColor = UIColor.gray
+        view.addSubview(playerView)
+
+        // MotionManagerを生成
+        playerMotionManager = CMMotionManager()
+        // 加速度の値を0.02秒ごとに取得
+        playerMotionManager.accelerometerUpdateInterval = 0.02
+
+        startAccelerometer()
     }
+    
+    // 迷路の1マスのViewを生成
+    func createView(x: Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat, offsetY: CGFloat) -> UIView {
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        let view = UIView(frame: rect)
+        
+        let center = CGPoint(x: offsetX + width * CGFloat(x), y: offsetY + height * CGFloat(y))
+        
+        view.center = center
+        
+        return view
+    }
+    
+    func startAccelerometer() {
+        // 加速度を取得
+        let handler: CMAccelerometerHandler = {(CMAccelerometerData: CMAccelerometerData?, error: Error?) -> Void in
+            self.speedX += CMAccelerometerData!.acceleration.x
+            self.speedY += CMAccelerometerData!.acceleration.y
+
+            // プレイヤーの中心位置を設定
+            var posX = self.playerView.center.x + (CGFloat(self.speedX) / 3)
+            var posY = self.playerView.center.y - (CGFloat(self.speedY) / 3)
+
+            // 画面上からプレイヤーがはみ出しそうなら、posX/Yを修正
+            if posX <= self.playerView.frame.width / 2 {
+                self.speedX = 0
+                posX = self.playerView.frame.width / 2
+            }
+            if posY <= self.playerView.frame.height / 2 {
+                self.speedY = 0
+                posY = self.playerView.frame.height / 2
+            }
+            if posX >= self.screenSize.width - (self.playerView.frame.width / 2) {
+                self.speedX = 0
+                posX = self.screenSize.width - (self.playerView.frame.width / 2)
+            }
+            if posY >= self.screenSize.height - (self.playerView.frame.height / 2) {
+                self.speedY = 0
+                posY = self.screenSize.height - (self.playerView.frame.height / 2)
+            }
+            
+            for wallRect in self.wallRectArray {
+                // playerViewとwallViewが当たっているかどうか
+                if wallRect.intersects(self.playerView.frame) {
+                    self.gameCheck(result: "gameover", message: "壁に当たりました")
+                    return
+                }
+            }
+
+            // plaerViewとgoalViewが当たっているかどうか
+            if self.goalView!.frame.intersects(self.playerView.frame) {
+                self.gameCheck(result: "clear", message: "クリアしました")
+                return
+            }
 
 
+            self.playerView.center = CGPoint(x: posX, y: posY)
+        }
+        // 加速度の開始
+        playerMotionManager.startAccelerometerUpdates(to: OperationQueue.main, withHandler: handler)
+    }
+    
+    func gameCheck(result: String, message: String) {
+        // 加速度を止める
+        if playerMotionManager.isAccelerometerActive {
+            playerMotionManager.stopAccelerometerUpdates()
+        }
+        
+        let gameCheckAlert: UIAlertController = UIAlertController(title: result, message: message, preferredStyle: .alert)
+        let retryAction = UIAlertAction(title: "もう一度", style: .default, handler: { (action: UIAlertAction!) -> Void in
+            self.retry()
+        })
+        
+        gameCheckAlert.addAction(retryAction)
+        
+        self.present(gameCheckAlert, animated: true, completion: nil)
+    }
+    
+    func retry() {
+        playerView.center = startView.center
+        if !playerMotionManager.isAccelerometerActive {
+            self.startAccelerometer()
+        }
+        speedX = 0.0
+        speedY = 0.0
+    }
 }
 
